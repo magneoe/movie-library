5b800d5877d048bfb44d7243f695c73aae81e66f
Magnus Østeng <mwo@it-minds.no>
2020-08-20 14:35:38 +0200

    Initial commit

---
+++ src/test/java/no/itminds/movielibrary/MovieLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package no.itminds.movielibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
